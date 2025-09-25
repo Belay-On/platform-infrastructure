397cc557ef4113d6bed5bc9334f500f4549e8686
Rachel Cope <rachel.cope@cnhind.com>
2025-09-25 13:09:24 -0600

    Adds workflow & updates main.tf & variables

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+# The terraform block is used to configure Terraform itself,
+# not your cloud resources. Here you declare the providers
+# (plugins) and their versions.
+terraform {
+  backend "gcs" {
+    bucket = var.gcp_bucket
+    prefix = var.environment
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_artifact_registry_repository" "belay_on_repo" {
+  location      = var.region
+  repository_id = "belay-on-docker-${var.environment}"
+  description   = "Belay On Docker Repository for ${var.environment}"
+  format        = "DOCKER"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  description = "Default GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "Default GCP zone"
+  type        = string
+  default     = "us-central1-c"
+}
+
+variable "gcp_project_id" {
+  description = "GCP Project ID"
+  type        = string
+  sensitive =  true
+}
+
+variable "gcp_bucket" {
+  description = "GCP Bucket name for Terraform state"
+  type        = string
+  sensitive =  true
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, stage, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "workload_identity_provider" {
+  description = "Path to GCP workload identity provider"
+  type        = string
+}
